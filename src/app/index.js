e4176b3150275b02a20145db7aed332ca4d3fb8c
Todarman <iamtodarman@gmail.com>
2017-07-09 22:28:02 +0530

    Register Component structuring and styling done, Validation checked for the fields

--- src/app/index.js
+++ src/app/index.js
@@ -20,33 +20,63 @@ class App extends Component{
 		super();
 		this.state={
 			email:"",
-			password:""
+			password:"",
+			username:"",
+			firstname:"",
+			lastname:"",
+			remail:"",
+			rpass:"",
+			rusername:"",
+			rcpass:"",
+			ragree:false,
+
 		}
-		this.handleChildData = this.handleChildData.bind(this);
+		this.handleLoginChildData = this.handleLoginChildData.bind(this);
+		this.handleRegisterChildData = this.handleRegisterChildData.bind(this);
 		this.onLoginClick =this.onLoginClick.bind(this);
+		this.onRegisterClick =this.onRegisterClick.bind(this);	
 	}
-	handleChildData(email,password){
-		this.setState({email});
-		this.setState({password});
+	handleLoginChildData(logindata){
+		this.setState({email:logindata.email});
+		this.setState({password:logindata.password});
+	}
+	handleRegisterChildData(registerdata){
+		this.setState({remail:registerdata.email});
+		this.setState({rpass:registerdata.password});
+		this.setState({firstname:registerdata.firstname});
+		this.setState({lastname:registerdata.lastname});
+		this.setState({rcpass:registerdata.cpassword});
+		this.setState({rusername:registerdata.lastname});
+		this.setState({ragree:registerdata.ragree});
 	}
 	onLoginClick(e){
 		console.log(e);
 		//make the api call and authenticate the user
 	}
+	onRegisterClick(e){
+		console.log(e);
+		//make the api call and authenticate the user
+	}
 	render(){
 		const classes = this.props.classes;
-		var data={heading:"Already have an Account? Login then!",
-		emailhelpertext:"Email format expected: john.doe@xyz.com",
-		passwordhelpertext:"Enter a strong password",emailplaceholder:"Enter emailid",passwordplaceholder:"Enter password"}
+		var logindata={heading:"Already have an Account? Login then!",
+		helpertext:{email:"Email format expected: john.doe@xyz.com",
+		password:"Enter a strong password"},placeholder:{email:"Enter emailid",password:"Enter password"}}; 
+		var registerData ={heading:"New to our site? Register now!"}
+
+		var registerdata={heading:"New to our site? Register now!",
+		helpertext:{firstname:"Lower and uppercase characters allowed",lastname:"Lower and uppercase characters allowed",username:"only Alphanumeric allowed",cpassword:"Confirm the above password",email:"Email format expected: john.doe@xyz.com",
+		password:"Enter a strong password"},placeholder:{firstname:"Enter your Firstname",lastname:"Enter your Lastname",username:"Enter your username",cpassword:"Confirm your password",email:"Enter emailid",password:"Enter password"}}; 
+
 		return(
-				<Grid container className={classes.container}>
-					<Grid item lg={4} md={6} sm={6} xs={12}>
-			          <Login data={data} updateParent={this.handleChildData} onLoginClick={this.onLoginClick}/>
-			        </Grid>
-			        <Grid item lg={4} md={6} sm={6} xs={12}>
-			          <Register/>
-			        </Grid>
-				</Grid>
+			<Grid container className={classes.container}>
+				<Grid item lg={4} md={6} sm={6} xs={12}>
+		          <Login data={logindata} updateParent={this.handleLoginChildData} onLoginClick={this.onLoginClick}/>
+		        </Grid>
+		        <Grid item lg={4} md={6} sm={6} xs={12}>
+		          <Register data ={registerdata} updateParent={this.handleRegisterChildData} onRegisterClick={this.onRegisterClick}/>
+		        </Grid>
+			</Grid>
 			)
 	}
 }
--- src/register/index.js
+++ src/register/index.js
@@ -1,7 +1,298 @@
 import React,{Component} from 'react'
+import Paper from 'material-ui/Paper';
+import Typography from 'material-ui/Typography';
+import TextField from 'material-ui/TextField';
+import PropTypes from 'prop-types';
+import Button from 'material-ui/Button';
+import { withStyles, createStyleSheet } from 'material-ui/styles';
+import validator from 'validator';
+import { LabelCheckbox } from 'material-ui/Checkbox';
+import { FormGroup } from 'material-ui/Form';
+var ValidatePassword = require('validate-password');
+var passvalidator = new ValidatePassword();
 
-export default class Register extends Component{
+const styleSheet = createStyleSheet('Register', theme => ({
+  registerContainer:{
+  	padding:20,
+  	display:'flex',
+  	flexDirection:'column',
+  	justifyContent:"center",
+  	alignItems:"center"
+  },
+  input:{
+  	width:"100%"
+  },
+  checkbox:{
+  	fontSize:12,
+  }
+}));
+
+class Register extends Component{
+	constructor(){
+		super();
+		this.state={
+			firstname:"",
+			lastname:"",
+			username:"",
+			email:"",
+			password:"",
+			cpassword:"",
+			firstnamehelpertext:"",
+			lastnamehelpertext:"",
+			emailhelpertext:"",
+			passwordhelpertext:"",
+			cpasswordhelpertext:"",
+			usernamehelpertext:"",
+			agreed:"",
+			fnameValid:false,
+			lnameValid:false,
+			unameValid:false,
+			emailValid:false,
+			passValid:false,
+			cpassValid:false
+		}
+		this.handlefirstnameData = this.handlefirstnameData.bind(this);
+		this.handlelastnameData = this.handlelastnameData.bind(this);
+		this.handleEmailData = this.handleEmailData.bind(this);
+		this.handleUsernameData = this.handleUsernameData.bind(this);
+		this.handlePasswordData = this.handlePasswordData.bind(this);
+		this.handleCPasswordData = this.handleCPasswordData.bind(this);
+		this.renderRegisterButton =this.renderRegisterButton.bind(this);
+
+	}
+	componentWillMount(){
+		let emailtext = this.props.data.helpertext.email;
+		let passtext = this.props.data.helpertext.password;
+		let cpasstext = this.props.data.helpertext.cpassword;
+		let fnametext = this.props.data.helpertext.firstname;
+		let lnametext = this.props.data.helpertext.lastname;
+		let unametext = this.props.data.helpertext.username;
+
+		this.setState({emailhelpertext:emailtext});
+		this.setState({passwordhelpertext:passtext});
+		this.setState({firstnamehelpertext:fnametext});
+		this.setState({lastnamehelpertext:lnametext});
+		this.setState({usernamehelpertext:unametext});
+		this.setState({cpasswordhelpertext:cpasstext});
+	}
+	handlefirstnameData(event){
+		let firstnameparams ={
+			firstname:event.target.value,
+			lastname:this.state.lastname,
+			email:this.state.email,
+			password: this.state.password,
+			cpassword:this.state.cpassword,
+			username:this.state.username
+		}
+		this.setState({firstname: event.target.value },this.props.updateParent(firstnameparams));
+		if(validator.isAlpha((firstnameparams.firstname))){
+			this.setState({firstnamehelpertext:"First name looks good"});
+			this.setState({fnameValid:true});
+		}
+		else{
+			this.setState({fnameValid:false});
+			this.setState({firstnamehelpertext:"Please enter a proper firstname"});
+		}
+	}
+	handlelastnameData(event){
+		let lastnameparams ={
+			firstname:this.state.firstname,
+			lastname:event.target.value,
+			email:this.state.email,
+			password: this.state.password,
+			cpassword:this.state.cpassword,
+			username:this.state.username
+		}
+		this.setState({lastname: event.target.value },this.props.updateParent(lastnameparams));
+		if(validator.isAlpha((lastnameparams.lastname))){
+			this.setState({lastnamehelpertext:"Last name looks good"});
+			this.setState({lnameValid:true});
+		}
+		else{
+			this.setState({lnameValid:false});
+			this.setState({lastnamehelpertext:"Please enter a proper lastname"});
+		}
+	}
+	handleEmailData(event){
+	 let emailparams ={
+			firstname:this.state.firstname,
+			lastname:this.state.lastname,
+			email:event.target.value,
+			password: this.state.password,
+			cpasswod:this.state.cpassword,
+			username:this.state.username
+		}
+	this.setState({ email: event.target.value },this.props.updateParent(emailparams));
+	 let EmailCheck = validator.isEmail(event.target.value);
+	 if (EmailCheck) {
+	 	this.setState({emailhelpertext:"Email looks good"});
+	 	this.setState({emailValid:true});
+	 }
+	 else{
+	 	this.setState({emailValid:false});
+	 	this.setState({emailhelpertext:"Email format expected: john.doe@xyz.com"});
+	 }
+	}
+	handleUsernameData(event){
+		let unparams ={
+			firstname:this.state.firstname,
+			lastname:this.state.lastname,
+			email:this.state.email,
+			password: this.state.password,
+			cpasswod:this.state.cpassword,
+			username:event.target.value
+		}
+	 this.setState({username: event.target.value },this.props.updateParent(unparams));
+	 if(validator.isAlphanumeric((unparams.username))){
+			this.setState({usernamehelpertext:"username looks good"});
+			this.setState({unameValid:true});
+		}
+		else{
+			this.setState({unameValid:false});
+			this.setState({usernamehelpertext:"Please enter a proper username"});
+		}
+	}
+	handlePasswordData(event){
+		let passparams ={
+			firstname:this.state.firstname,
+			lastname:this.state.lastname,
+			email:this.state.email,
+			password: this.state.password,
+			cpasswod:this.state.cpassword,
+			username:event.target.value
+		}
+	 this.setState({ password: event.target.value },this.props.updateParent(passparams));
+	 let val = event.target.value;
+	 var passwordData = passvalidator.checkPassword(val);
+	 if (passwordData.isValid) {
+	 	this.setState({passValid:passwordData.isValid})
+	 	this.setState({passwordhelpertext:"Password looks good"});
+	 }
+	 else{
+	 	this.setState({passValid:passwordData.isValid})
+	 	this.setState({passwordhelpertext:passwordData.validationMessage});
+	 }
+	}
+	handleCPasswordData(event){
+		let passparams ={
+			firstname:this.state.firstname,
+			lastname:this.state.lastname,
+			email:this.state.email,
+			password: this.state.password,
+			cpasswod:this.state.cpassword,
+			username:event.target.value,
+		}
+	 this.setState({ cpassword: event.target.value },this.props.updateParent(passparams));
+	 let val = event.target.value;
+	 var passwordData = passvalidator.checkPassword(val);
+	 if (passwordData.isValid && (this.state.password === val)) {
+	 	this.setState({cpassValid:passwordData.isValid})
+	 	this.setState({cpasswordhelpertext:"Password Confirmed"});
+	 }
+	 else{
+	 	this.setState({cpassValid:passwordData.isValid})
+	 	this.setState({cpasswordhelpertext:passwordData.validationMessage});
+	 }
+	}
+
+	renderRegisterButton(classes){
+		if(this.state.emailValid && this.state.fnameValid && this.state.lnameValid && this.state.unameValid && this.state.passValid && this.state.cpassValid && this.state.agreed){
+	        return [<Button key={Math.random(3)+2} raised color="primary" onClick={this.props.onRegisterClick} className={classes.button}>
+	        	Register
+	      	</Button>];
+		}else{
+	      	return [<Button key={Math.random(3)+2} raised color="primary" disabled className={classes.button}>
+	        	Register
+	      	</Button>];
+		}
+	}
 	render(){
-		return(<div>This is register</div>)
+		const classes = this.props.classes
+		return(
+			<Paper className={classes.registerContainer} elevation={4}>
+		        <Typography type="title" component="p">
+		          {this.props.data.heading}
+		        </Typography>
+		        <TextField
+		          id="firstname"
+		          className={classes.input}
+		          label={this.props.data.placeholder.firstname}
+		          value={this.state.firstname}
+		          onChange={this.handlefirstnameData}
+		          type="text"
+		          helperText={this.state.firstnamehelpertext}
+		          marginForm
+		        />
+		        <TextField
+		          id="lastname"
+		          className={classes.input}
+		          label={this.props.data.placeholder.lastname}
+		          value={this.state.lastname}
+		          onChange={this.handlelastnameData}
+		          type="text"
+		          helperText={this.state.lastnamehelpertext}
+		          marginForm
+		        />
+		        <TextField
+		          id="username"
+		          className={classes.input}
+		          label={this.props.data.placeholder.username}
+		          value={this.state.username}
+		          onChange={this.handleUsernameData}
+		          type="text"
+		          helperText={this.state.usernamehelpertext}
+		          marginForm
+		        />
+		        <TextField
+		          id="remail"
+		          className={classes.input}
+		          label={this.props.data.placeholder.email}
+		          value={this.state.email}
+		          onChange={this.handleEmailData}
+		          type="email"
+		          helperText={this.state.emailhelpertext}
+		          marginForm
+		        />
+		        <TextField
+		          id="rpassword"
+		          className={classes.input}
+		          label={this.props.data.placeholder.password}
+		          type="password"
+		          value={this.state.password}
+		           onChange={this.handlePasswordData}
+		          helperText={this.state.passwordhelpertext}
+		          marginForm
+		        />
+		        <TextField
+		          id="rcpassword"
+		          className={classes.input}
+		          label={this.props.data.placeholder.cpassword}
+		          type="password"
+		          value={this.state.cpassword}
+		           onChange={this.handleCPasswordData}
+		          helperText={this.state.cpasswordhelpertext}
+		          marginForm
+		        />
+		        <FormGroup row>
+		        	<Typography type="caption" component="p">
+			          <LabelCheckbox
+				          checked={this.state.checkedA}
+				          className={classes.checkbox}
+				          onChange={(event, checked) => this.setState({ agreed: checked })}
+				          label="By registering I agree to the terms and conditions"
+				          value="agreed"
+				        />
+			        </Typography>
+			    </FormGroup>
+			    {this.renderRegisterButton(classes)}
+		    </Paper>
+			)
 	}
 }
+
+
+Register.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styleSheet)(Register);
